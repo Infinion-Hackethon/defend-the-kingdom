cff1932bc310f2f0f24c5bf7773e70718ee30afe
Naveen <allena15243@gmail.com>
2024-02-16 06:38:39 +0530

    Added parser

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <pugixml.hpp>
+#include "pugixml.hpp"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -9,11 +9,19 @@ int main(int argc, char* argv[]) {
 
     // Get command line arguments one by one
     std::string xmlFilePath = argv[1];
+    
+    pugi::xml_document doc;
 
-
-    // Delete the below line, and write your code, use of OOPs concepts are preferred...
-
-    std::cout << "The function is not fully implemented.\n";
+    // read xml input from argument
+    pugi::xml_parse_result result = doc.load_file(xmlFilePath.c_str());
+    if (!result) {
+        std::cout << "XML file not loaded successfully.\n";
+    }
+    else {
+        std::cout << "XML file loaded successfully.\n";
+        // cout node text
+        std::cout << doc.child("Kingdom").child("Name").text().as_string() << std::endl;
+    }
 
     return 0;
 }
