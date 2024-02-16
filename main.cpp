514164c3abc2d8772c8529bb4a42170a2cf1253c
Aditya Dhanraj <opsingh861@gmail.com>
2024-02-16 12:32:38 +0530

    cleaned files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
-#include <pugixml.hpp>
-#include <Kingdom.h>
+#include "Kingdom.h"
+using namespace std;
+
+void printSoldier(const Soldier &soldier)
+{
+    std::cout << "    Int_Class_ID: " << soldier.intClassID << std::endl;
+    std::cout << "    Name: " << soldier.name << std::endl;
+    std::cout << "    Active: " << soldier.active << std::endl;
+    std::cout << "    SkillRef: " << soldier.skillRef << std::endl;
+}
+
+void printClan(const Clan &clan)
+{
+    std::cout << "  Int_Class_ID: " << clan.intClassID << std::endl;
+    std::cout << "  Name: " << clan.name << std::endl;
+    std::cout << "  MaxCloningPower: " << clan.maxCloningPower << std::endl;
+    std::cout << "  CloningVar: " << clan.cloningVar << std::endl;
+    std::cout << "  BaseDeployCost: " << clan.baseDeployCost << std::endl;
+    std::cout << "  Soldiers: " << std::endl;
+    for (const auto &soldier : clan.soldiers)
+    {
+        printSoldier(soldier);
+    }
+}
+
+void printSkill(const Skill &skill)
+{
+    std::cout << "  Int_Class_ID: " << skill.intClassID << std::endl;
+    std::cout << "  Strength: " << skill.strength << std::endl;
+}
+
+void printKingdom(const Kingdom &kingdom)
+{
+    std::cout << "Name: " << kingdom.name << std::endl;
+
+    std::cout << "Clans: " << std::endl;
+    for (const auto &clan : kingdom.clans)
+    {
+        printClan(clan);
+    }
+
+    std::cout << "Skills: " << std::endl;
+    for (const auto &skill : kingdom.skills)
+    {
+        printSkill(skill);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,12 +55,21 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // Get command line arguments one by one
-    std::string xmlFilePath = argv[1];
+    string filePath = argv[1];
 
-    // Delete the below line, and write your code, use of OOPs concepts are preferred...
+    try
+    {
+        Kingdom kingdom(filePath);
+        // Now you can use the 'kingdom' object as needed
+        printKingdom(kingdom);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 
-    std::cout << "The function is not fully implemented.\n";
+    cout << "Press Enter to continue...";
+    cin.ignore();
 
     return 0;
 }
