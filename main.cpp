5c415ad8cc976ea5f35feba82419120e016bf5cc
Abhay Sahani <107034831+its-AbhaySahani@users.noreply.github.com>
2024-02-16 10:53:44 +0530

    Create main.cpp
    Initiated

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <pugixml.hpp>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "XML file path not provided.\n";
+        exit(1);
+    }
+
+    // Get command line arguments one by one
+    std::string xmlFilePath = argv[1];
+
+
+    // Delete the below line, and write your code, use of OOPs concepts are preferred...
+
+    std::cout << "The function is not fully implemented.\n";
+
+    return 0;
+}
